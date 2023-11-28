3ea5a500d29774f2327ccf134df92506f72767ed
Mehmet Mikayil Karaman <mkaraman48@icloud.com>
2023-11-28 14:09:34 +0300

    Namespace_example

---
+++ Namespace/nested/nested_02.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+namespace Enc {
+
+	void foo(int)
+	{
+		std::cout << "Enc::foo(int)\n";
+	}
+
+	namespace Nested {
+		void foo()
+		{
+			std::cout << "Enc::Nested::foo()\n";
+		}
+
+		void func()
+		{
+			//foo(10); //invalid
+			foo();
+			Enc::foo(10);
+		}
+
+	}
+}
+
+int main()
+{
+	Enc::foo(0);
+	Enc::Nested::func();
+}

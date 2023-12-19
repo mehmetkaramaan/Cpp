a80b2e8e789e5aae57fa53e3bffbcca75c8b3235
Mehmet Mikayil Karaman <mkaraman48@icloud.com>
2023-12-19 18:01:34 +0300

    Delete OOP.cpp

--- OOP.cpp
+++
@@ -1,9 +0,0 @@
-/*
-"delete" Bildirimi:
-
--> Bir fonksiyonun delete bildirimi fonksiyonun var ancak yapılan çağrının geçersiz olduğunu söyler.
-
--> Delete edilen function vardır ve function overload resolution'a dahil edilir. 
-
-*/
-  
